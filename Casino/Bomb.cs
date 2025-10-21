21c56fc8ba51cc09ef3be9225cd17bff7da828e0
OstapiukOlexandr <portalvox17@gmail.com>
2025-10-21 21:44:21 +0300

    Interface

---
+++ Casino/Bomb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Casino
+{
+    public partial class Bomb : Form
+    {
+        public Bomb()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            bmp.MakeTransparent(Color.White); // робимо білий фон прозорим
+            pictureBox1.Image = bmp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Casino/Form1.cs
@@ -0,0 +1,41 @@
+namespace Casino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Bomb bombForm = new Bomb();
+
+            // Показуємо її
+            bombForm.Show();
+
+            // (необов’язково) ховаємо головну форму Casino
+            this.Hide();
+        }
+    }
+}
